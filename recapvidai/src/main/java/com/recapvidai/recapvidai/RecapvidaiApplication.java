1af97074c7dd916caaa7136c3dbd20b49586e303
DaanishHindustaniTR <daanishhindustani@yahoo.com>
2025-06-13 21:41:55 -0500

    Initial commit of boiler plate code.

---
+++ recapvidai/src/main/java/com/recapvidai/recapvidai/RecapvidaiApplication.java
@@ -0,0 +1,13 @@
+package com.recapvidai.recapvidai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecapvidaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecapvidaiApplication.class, args);
+    }
+
+}
---
+++ recapvidai/src/test/java/com/recapvidai/recapvidai/RecapvidaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recapvidai.recapvidai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecapvidaiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
